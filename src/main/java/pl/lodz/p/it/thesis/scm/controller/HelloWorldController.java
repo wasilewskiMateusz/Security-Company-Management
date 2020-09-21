6fe24482522a11cdc82717b161b0a029f5fbe5c3
Mateusz Wasilewski <wasilewskimateusz1998@gmail.com>
2020-09-21 15:58:45 +0200

    Add jwt utill class

---
+++ src/main/java/pl/lodz/p/it/thesis/scm/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package pl.lodz.p.it.thesis.scm.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping({"/hello"})
+    public String firstPage() {
+        return "Hello world";
+    }
+}
